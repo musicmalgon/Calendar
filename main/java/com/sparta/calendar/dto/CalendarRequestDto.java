a4ffc1b8842f6e92234debb3169a50e55494478c
musicmalgon <113411490+musicmalgon@users.noreply.github.com>
2024-10-04 11:22:00 +0900

    Add files via upload

---
+++ main/java/com/sparta/calendar/dto/CalendarRequestDto.java
@@ -0,0 +1,10 @@
+package com.sparta.calendar.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CalendarRequestDto {
+    private String task;
+    private String username;
+    private String password;
+}
